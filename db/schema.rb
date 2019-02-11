4451cfac34346dc7192bd58803af308275c69733
bae.jeonghan <jeonghan-bae@united.jp>
2019-02-11 10:05:51 +0000

    have button add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190210155447) do
+ActiveRecord::Schema.define(version: 20190211091159) do
+
+  create_table "haveships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "type"
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_haveships_on_item_id", using: :btree
+    t.index ["user_id"], name: "index_haveships_on_user_id", using: :btree
+  end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "code"
@@ -39,6 +49,8 @@ ActiveRecord::Schema.define(version: 20190210155447) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "haveships", "items"
+  add_foreign_key "haveships", "users"
   add_foreign_key "ownerships", "items"
   add_foreign_key "ownerships", "users"
 end
