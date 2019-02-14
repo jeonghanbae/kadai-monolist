7ec4a123f1db165dd689d9f551cd9a71f5346afb
bae.jeonghan <jeonghan-bae@united.jp>
2019-02-14 20:28:25 +0000

    ranking have

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,11 @@
 class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
-    @items = Item.find(@ranking_counts.keys)
+    @items = Item.find(@ranking_counts.keys)    
+  end
+
+  def have
+    @ranking_counts = Have.haveranking
+    @items = Item.find(@ranking_counts.keys)    
   end
 end
